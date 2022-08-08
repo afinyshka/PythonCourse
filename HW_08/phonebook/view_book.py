8cd61b0abdaec901f57c6f617c3f215c24cb372c
afinyshka <afinyshka@mail.ru>
2022-08-08 11:59:25 +0300

    change label to text and add scroll

--- HW_08/phonebook/view_book.py
+++ HW_08/phonebook/view_book.py
@@ -17,32 +17,48 @@ def get_entry_delete(del_1, del_2, val_1, val_2, val_3, val_4):
 
 def find_entry(texter):
     value = input_line.get()
-    output_line_1 = tk.Label(win, text = texter, bg="Gray", justify=tk.RIGHT)
+    output_line_1 = tk.Label(win, relief=tk.SUNKEN, text = texter, bg="Gray", justify=tk.RIGHT)
     if len(value) > 0:
         output_text = logic.find_contact(value)
-    output_line_2 = tk.Label(win, text=output_text, bg="Gray", justify=tk.LEFT, padx=2, pady=2)
+    
+    output_line_2 = tk.Text(win, font="Arial 13", height = 6, width = 45, bg= "Gray")
     output_line_1.grid(row=1, column=0, columnspan=2, stick="wesn")
-    output_line_2.grid(row=2, column=0, columnspan=2, rowspan=4, stick="wesn", padx=2, pady=2)
+    output_line_2.grid(row=2, column=0, columnspan=2, rowspan=4, stick="sn")
+    sc = tk.Scrollbar(win, orient=tk.VERTICAL)
+    sc.grid(row=2, column=1, rowspan=4, sticky=tk.NS+tk.E)
+    sc.config(command=output_line_2.yview)
+    output_line_2.config(yscrollcommand=sc.set)
+    sc.config(command=output_line_2.yview)
+    output_line_2.insert(1.0, output_text)
     return output_text
+
 
 def find_entry_del_widget(texter):
     value = input_line.get()
-    output_line_1 = tk.Label(win, text = texter, bg="Gray", justify=tk.RIGHT)
-    if len(value) > 0:
-        output_text = logic.find_contact(value)
-    output_line_2 = tk.Label(win, text=output_text, bg="Gray", justify=tk.LEFT, padx=2, pady=2)
+    output_line_1 = tk.Label(win, relief=tk.SUNKEN, text = texter, bg="Gray", justify=tk.RIGHT)
+    # if len(value) > 0:
+    output_text = logic.find_contact(value)
+    
+    output_line_2 = tk.Text(win, font="Arial 13", height = 6, width = 45, bg= "Gray")
     output_line_1.grid(row=1, column=0, columnspan=2, stick="wesn")
-    output_line_2.grid(row=2, column=0, columnspan=2, rowspan=4, stick="wesn", padx=2, pady=2)
-    clear_1 = tk.Button(win, text = "Clear", command=lambda: widgets_remove(output_line_1, output_line_2, clear_1))
+    output_line_2.grid(row=2, column=0, columnspan=2, rowspan=4, stick="sn")
+    sc = tk.Scrollbar(win, orient=tk.VERTICAL)
+    sc.grid(row=2, column=1, rowspan=4, sticky=tk.NS+tk.E)
+    sc.config(command=output_line_2.yview)
+    output_line_2.config(yscrollcommand=sc.set)
+    sc.config(command=output_line_2.yview)
+    output_line_2.insert(1.0, output_text)
+    clear_1 = tk.Button(win, text = "Clear", command=lambda: widgets_remove(output_line_1, output_line_2, clear_1, sc))
     clear_1.grid(row=6, column=1, stick="we")
 
-def widgets_remove(output_line_1, output_line_2, clear_1):
+def widgets_remove(output_line_1, output_line_2, clear_1, sc):
     output_line_1.grid_remove()
     output_line_2.grid_remove()
+    sc.grid_remove()
     clear_1.grid_forget()
 
 def add_entry():
-    widgets_remove(output_line_1, output_line_2, clear_1)
+    # widgets_remove(output_line_1, output_line_2, clear_1)
     tk.Label(win, text='Enter first name:', justify=tk.RIGHT).grid(row=1, column=0, stick="wesn",)
     tk.Label(win, text='Enter last name:').grid(row=2, column=0, stick="wesn")
     tk.Label(win, text='Enter phone number:').grid(row=3, column=0, stick="wesn")

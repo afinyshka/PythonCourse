7ebf755a32ff6a6ef8ad078bc6add37f3122289f
afinyshka <afinyshka@mail.ru>
2022-08-15 17:13:43 +0300

    bot.py still not work properly

--- HW_09/02_telegram_bot/bot.py
+++ HW_09/02_telegram_bot/bot.py
@@ -8,7 +8,7 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 from aiogram.dispatcher import FSMContext
 
 from config import TOKEN
-from xo_game import game_X_O
+from xo_game import matrix_text, win_combination_check, winning_positions, get_move
 
 bot = Bot(token=TOKEN)
 # dp = Dispatcher(bot)
@@ -18,63 +18,78 @@ dp = Dispatcher(bot, storage=storage)
 print('Bot is initialiezed...')
 
 available_moves = ['1','2','3','4','5','6','7','8','9']
+user_data = []
 
 class MoveState(StatesGroup):
-    move = State()
+    waiting_for_move_x = State()
+    waiting_for_move_o = State()
+
+def get_keyboard():
+    buttons = available_moves
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(*buttons)
+    return keyboard
 
 @dp.message_handler(commands=['game'])
-async def user_register(message: types.Message):
+async def star_game(message: types.Message):
     await message.answer("You initialized XO game! ")
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    buttons = available_moves
-    keyboard.add(*buttons)
-    await message.answer("Choose the cell:  ", reply_markup=keyboard)
+    keyboard = get_keyboard()
+    await message.answer('Choose the cell for "X":  ', reply_markup=keyboard)
+    await MoveState.waiting_for_move_x.set()
     
-    await MoveState.move.set()
-
-# @dp.message_handler(commands=['move'])
-# async def user_move(message: types.Message):
-#     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-#     buttons = available_moves
-#     keyboard.add(*buttons)
-#     await message.answer("Choose the cell:  ", reply_markup=keyboard)
-#     # await MoveState.move.set()
-
-
-# Обратите внимание: есть второй аргумент
-@dp.message_handler(state=MoveState.move)
-async def move_chosen(message: types.Message, state: FSMContext):
+@dp.message_handler(state=MoveState.waiting_for_move_x)
+async def user_move_x(message: types.Message, state: FSMContext):
     if message.text.lower() not in available_moves:
-        await message.answer("Please, choose the button from the keybord low.")
+        await message.answer("Please use the bellow keyboard.")
         return
-    print(message.text)
-    await state.update_data(move=message.text.lower())
-    await message.answer(f"Great! Your move is initialized... it's {message.text}", reply_markup=types.ReplyKeyboardRemove())
-    await message.answer(text = game_X_O(message.text))
-    return message.text
+    await state.update_data(move_x=message.text)
+    user_data = await state.get_data()
+    print("massage text: ",message.text)
+    keyboard = get_keyboard()
+    await MoveState.next()
+    await message.answer('Choose the cell for "O":  ', reply_markup=keyboard)
 
+@dp.message_handler(state=MoveState.waiting_for_move_o)   
+async def user_move_o(message: types.Message, state: FSMContext):
+    if message.text.lower() not in available_moves:
+        await message.answer("Please use the bellow keyboard.")
+        return
+    await state.update_data(move_o=message.text)
+    user_data = await state.get_data()
+    print("massage text: ",message.text)
+    print(user_data, type(user_data))
+    game_X_O(user_data)
+    await message.answer(f"Your first move is {user_data['move_x']}, second is {user_data['move_o']} = {message.text.lower()}", reply_markup=types.ReplyKeyboardRemove())
+    await state.finish()
 
+@dp.message_handler(commands="cancel", state="*")
+async def cmd_cancel(message: types.Message, state: FSMContext):
+    await state.finish()
+    await message.answer("Действие отменено", reply_markup=types.ReplyKeyboardRemove())
 
-    # keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    # for size in available_food_sizes:
-    #     keyboard.add(size)
-    # # Для последовательных шагов можно не указывать название состояния, обходясь next()
-    # await OrderFood.next()
-    # await message.answer("Теперь выберите размер порции:", reply_markup=keyboard)
-
-
-
-# @dp.message_handler(state=MoveState.move)
-# async def get_move(message: types.Message, state: FSMContext):
-#     await state.update_data(move=message.text)
-#     print(message.text)
-#     await message.answer(f"Great! Your move is initialized...it's {message.text}", reply_markup=types.ReplyKeyboardRemove())
-#     await message.answer(matrix_text(available_moves))
-
-
-
-
-
+def game_X_O (user_data: dict, rows: int = 3, columns: int = 3):
+    print("You started the xo game")
+    list_x_o = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+    move_storage = []
+    count = 0 # Number of players moves
+    print(matrix_text(list_x_o))
+    while count < (rows * columns):
+        if count%2==0:
+            move = int(user_data['move_x']) # если вернуть функцию то выполнится проверка и дальше функция не пойдет
+            print("move", move)
+        else:
+            move = int(user_data['move_o']) # если вернуть функцию то выполнится проверка и дальше функция не пойдет
+            print("move", move)
+        move_storage.append(move+1)
+        print(move_storage)
+        print(matrix_text(list_x_o))
+        count += 1
+        if count > (rows + columns - 2) and (win_combination_check(list_x_o) == 'win'):
+            break
+        if count == 9:
+            print ('You ended the game in a draw!')
+    print('The end!')
+    return matrix_text(list_x_o)
 
 # Создаем message_handler и объявляем там функцию ответа:
 @dp.message_handler(commands=['start'])
@@ -94,14 +109,12 @@ async def process_time_command(message: types.Message):
 
 @dp.message_handler(commands=['matrix'])
 async def process_matrix_command(message: types.Message):
-    list_x_o = message.from_user.id
-    # list_x_o = [["-", "-", "-"], ["-", "-", "-"], ["-", "-", "-"]]
-    await message.reply(matrix_text(list_x_o)) # просто сообщение от бота
+    list_x_o = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+    await message.answer(matrix_text(list_x_o)) # просто сообщение от бота
 
-@dp.message_handler()
-async def echo_message(msg: types.Message):
-    await bot.send_message(msg.from_user.id, msg.text) # просто сообщение от бота
+# @dp.message_handler()
+# async def echo_message(msg: types.Message):
+#     await bot.send_message(msg.from_user.id, msg.text) # просто сообщение от бота
 
 if __name__ == '__main__':
     executor.start_polling(dp)
-    

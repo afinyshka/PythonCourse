e97154f61d6f5ab7ed347543c8b7b3963a0c6d63
afinyshka <afinyshka@mail.ru>
2022-08-16 14:38:03 +0300

    add requirements

--- HW_09/02_telegram_bot/how_to_venv.py
+++ HW_09/02_telegram_bot/how_to_venv.py
@@ -1,2 +1,3 @@
 # python3 -m venv .venv - создать виртуальное окружение в текущем каталоге
 # source .venv/bin/activate - переключение на виртуальное окружение
+# pip install -r requirements.txt - создание текстового файла с бибилиотеками
